8b659ff1c84d5b521b913cf9610fe9dd15f48f7e
Vladimir Voroshilov <voroshil@gmail.com>
2017-03-25 14:14:25 +0600

    fix compilation errors

--- examples/stm32/f1/pinboard-ii/lcd_hd44780/hd44780.c
+++ examples/stm32/f1/pinboard-ii/lcd_hd44780/hd44780.c
@@ -1,12 +1,12 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <inttypes>
+#include <inttypes.h>
 
 #include "hd44780.h"
 #include "hd44780_pins.h"
 
-uint16_t _hd44780_read(void);
-void _hd44780_write(uint16_t gpios);
+static uint16_t _hd44780_read(void);
+static void _hd44780_write(uint16_t gpios);
 
 #if defined(LCD_BUS_4BIT) && defined(LCD_BUS_8BIT)
 #error Only one of LCD_BUS_4BIT or LCD_BUS_8BIT should be defined
@@ -15,20 +15,20 @@ void _hd44780_write(uint16_t gpios);
 #define LCD_ALL_DATA_PINS  (LCD_D0_PIN | LCD_D1_PIN | LCD_D2_PIN | LCD_D3_PIN | LCD_D4_PIN | LCD_D5_PIN | LCD_D6_PIN | LCD_D7_PIN)
 #define LCD_CMD_INIT(lines,height)   LCD_CMD_MODE(1, lines, height)
 
-void hd44780_write(uint8_t data){
-  uint16_t gpios = (data & 1 ? LCD_D0_PIN) | 
-  (data & 2 ? LCD_D1_PIN)|
-  (data & 4 ? LCD_D2_PIN)|
-  (data & 8 ? LCD_D3_PIN)|
-  (data & 16 ? LCD_D4_PIN)|
-  (data & 32 ? LCD_D4_PIN)|
-  (data & 64 ? LCD_D4_PIN)|
-  (data & 128 ? LCD_D5_PIN);
+static void hd44780_write(uint8_t data){
+  uint16_t gpios = (data & 1 ? LCD_D0_PIN:0) | 
+  (data & 2 ? LCD_D1_PIN:0)|
+  (data & 4 ? LCD_D2_PIN:0)|
+  (data & 8 ? LCD_D3_PIN:0)|
+  (data & 16 ? LCD_D4_PIN:0)|
+  (data & 32 ? LCD_D4_PIN:0)|
+  (data & 64 ? LCD_D4_PIN:0)|
+  (data & 128 ? LCD_D5_PIN:0);
 
   _hd44780_write(gpios);
 }
 
-uint8_t hd44780_read(void){
+static uint8_t hd44780_read(void){
   uint16_t gpios = _hd44780_read();
 
   return (gpios & LCD_D0_PIN ? 1 : 0) |
@@ -45,7 +45,7 @@ uint8_t hd44780_read(void){
 #define LCD_ALL_DATA_PINS  (LCD_D0_PIN | LCD_D1_PIN | LCD_D2_PIN | LCD_D3_PIN)
 #define LCD_CMD_INIT(lines,height)   LCD_CMD_MODE(0, lines, height)
 
-void hd44780_write(uint8_t data){
+static void hd44780_write(uint8_t data){
   uint16_t gpios = (data & 1 ? LCD_D0_PIN) | 
   (data & 2 ? LCD_D1_PIN)|
   (data & 4 ? LCD_D2_PIN)|
@@ -58,7 +58,7 @@ void hd44780_write(uint8_t data){
   _hd44780_write(gpios);
 }
 
-uint8_t hd44780_read(void){
+static uint8_t hd44780_read(void){
   uint16_t gpios = _hd44780_read();
   uint8_t res = 0;
 
@@ -80,49 +80,61 @@ uint8_t hd44780_read(void){
 #error Either LCD_BUS_4BIT or LCD_BUS_8BIT should be defined
 #endif
 
+void hd44780_wait(void){
+  uint8_t data;
+
+  gpio_clear(LCD_CTRL_BANK, LCD_RS_PIN);
+  do{
+    data = hd44780_read();
+  }while(data & 0x80);  
+}
 static void _hd44780_write(uint16_t gpios){
 
-  gpio_set_mode(GLCD_DATA_BANK, GPIO_MODE_OUTPUT_50_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, ALL_DATA_PINS);
+  gpio_set_mode(LCD_DATA_BANK, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, LCD_ALL_DATA_PINS);
 
-  gpio_set(LCD_CTRL_BANK, LCD_E_PIN);
 
   gpio_set(LCD_DATA_BANK, gpios & LCD_ALL_DATA_PINS);
   gpio_clear(LCD_DATA_BANK, (~gpios) & LCD_ALL_DATA_PINS);
 
+  gpio_set(LCD_CTRL_BANK, LCD_E_PIN);
+  _delay_us(2);
   gpio_clear(LCD_CTRL_BANK, LCD_E_PIN);
 
   gpio_set_mode(LCD_DATA_BANK, GPIO_MODE_OUTPUT_50_MHZ,
-		      GPIO_CNF_OUTPUT_OPENDRAIN, ALL_DATA_PINS);
-  gpio_set(LCD_DATA_BANK, ALL_DATA_PINS);
+		      GPIO_CNF_OUTPUT_OPENDRAIN, LCD_ALL_DATA_PINS);
+  gpio_set(LCD_DATA_BANK, LCD_ALL_DATA_PINS);
 }
 
 static uint16_t _hd44780_read(void){
   uint16_t gpios = 0;
 
   gpio_set_mode(LCD_DATA_BANK, GPIO_CNF_INPUT_PULL_UPDOWN,
-		      GPIO_MODE_INPUT, ALL_DATA_PINS);
+		      GPIO_MODE_INPUT, LCD_ALL_DATA_PINS);
 
   gpio_set(LCD_CTRL_BANK, LCD_RW_PIN);
-  gpio_set(LCD_CTRL_BANK, LCD_E_PIN);
 
-  gpios = gpio_get(LCD_DATA_BANK, LCD_ALL_DATA_PINS);
-  
+  gpio_set(LCD_CTRL_BANK, LCD_E_PIN);
+  _delay_us(2);
+  gpios = gpio_get(LCD_DATA_BANK, LCD_ALL_DATA_PINS);  
   gpio_clear(LCD_CTRL_BANK, LCD_E_PIN);
+
   gpio_clear(LCD_CTRL_BANK, LCD_RW_PIN);
 
   gpio_set_mode(LCD_DATA_BANK, GPIO_MODE_OUTPUT_50_MHZ,
-		      GPIO_CNF_OUTPUT_OPENDRAIN, ALL_DATA_PINS);
-  gpio_set(LCD_DATA_BANK, ALL_DATA_PINS);
+		      GPIO_CNF_OUTPUT_OPENDRAIN, LCD_ALL_DATA_PINS);
+  gpio_set(LCD_DATA_BANK, LCD_ALL_DATA_PINS);
 
   return gpios;
 }
 
 void hd44780_command(uint8_t data){
+  hd44780_wait();
   gpio_clear(LCD_CTRL_BANK, LCD_RS_PIN);
   hd44780_write(data);
 }
 void hd44780_char(uint8_t data){
+  hd44780_wait();
   gpio_set(LCD_CTRL_BANK, LCD_RS_PIN);
   hd44780_write(data);
 }
@@ -145,15 +157,16 @@ void hd44780_init(){
 
   gpio_set_mode(LCD_DATA_BANK, GPIO_MODE_OUTPUT_50_MHZ,
 		      GPIO_CNF_OUTPUT_OPENDRAIN, LCD_ALL_DATA_PINS);
-  gpio_set(LCD_DATA_BANK, ALL_DATA_PINS);
+  gpio_set(LCD_DATA_BANK, LCD_ALL_DATA_PINS);
 
   hd44780_command(LCD_CMD_INIT(1, 0)); // 2-lines, 5x8
+  _delay_us(40);
   hd44780_clear();
-  hd44780_command(LCD_CMD_AUTO_INC_CUR);
+  hd44780_command(LCD_CMD_AUTO_MOVE(1, 0));
 }
 
 void hd44780_onoff(uint8_t screen_on, uint8_t cursor_dash, uint8_t cursor_blink){
-  hd44780_command(LCD_CMD_ON(screen_on, cursor_dash, blink));
+  hd44780_command(LCD_CMD_ON(screen_on, cursor_dash, cursor_blink));
 }
 void hd44780_goto(uint8_t x, uint8_t y){
   hd44780_command(LCD_CMD_USE_DDRAM((((y)& 1)*0x40)+((x) & 15)));
--- examples/stm32/f1/pinboard-ii/lcd_hd44780/lcd_hd44780.c
+++ examples/stm32/f1/pinboard-ii/lcd_hd44780/lcd_hd44780.c
@@ -1,24 +1,38 @@
+#include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 
-void gpio_setup(void){
-}
+#include "hd44780.h"
+
 static void clock_setup(void)
 {
   rcc_clock_setup_in_hse_12mhz_out_72mhz();
+
+  rcc_periph_clock_enable(RCC_TIM2);
+  rcc_periph_reset_pulse(RST_TIM2);
+  timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+  timer_set_prescaler(TIM2, 71); // 1MHz
+  timer_one_shot_mode(TIM2);
 }
 
-void lcd_init(void){
+void _delay_us(uint16_t us){
+  TIM_ARR(TIM2) = us; // Period = us
+  TIM_CNT(TIM2) = 0;
+  TIM_CR1(TIM2) |= TIM_CR1_CEN;
+  while (TIM_SR(TIM2) & TIM_CR1_CEN);
+}
+
+static void lcd_init(void){
   hd44780_init();
   hd44780_onoff(1,0,0); // Screen on, cursor off, no blinking
 }
 
-void main(void){
+int main(void){
   clock_setup();
-  gpio_setup();
 
   lcd_init();
   
   while(1){
   }
+  return 0;
 }
